e53d0619c5059034c5117e3ddb508ccc3d4f6fbb
vinivendra <vinivendra@gmail.com>
2016-12-04 12:45:14 -0200

    Added target to XCode.

---
+++ Chapter 2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Chapter 2
+//
+//  Created by Vinicius Vendramini on 12/4/16.
+//  Copyright © 2016 Vinicius Vendramini. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

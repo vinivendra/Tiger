6bc0c7418e0c262cdeb2be7f73d335a1ca7780a3
vinivendra <vinivendra@gmail.com>
2016-12-04 22:26:53 -0200

    Added basic testing structure.

---
+++ Chapter 2 Tests/Chapter_2_Tests.swift
@@ -0,0 +1,13 @@
+import XCTest
+
+class Chapter2Tests: XCTestCase {
+
+	override func setUp() {
+		super.setUp()
+		parse(file: "Chapter 2/test.tig")
+	}
+
+	func testComments() {
+
+	}
+}
--- Chapter 2/main.swift
+++ Chapter 2/main.swift
@@ -1,17 +1,18 @@
 // TODO: Add tests?
-
-let toknames = [
-	"ID", "STRING", "INT", "COMMA", "COLON", "SEMICOLON", "LPAREN",
-	"RPAREN", "LBRACK", "RBRACK", "LBRACE", "RBRACE", "DOT", "PLUS",
-	"MINUS", "TIMES", "DIVIDE", "EQ", "NEQ", "LT", "LE", "GT", "GE",
-	"AND", "OR", "ASSIGN", "ARRAY", "IF", "THEN", "ELSE", "WHILE", "FOR",
-	"TO", "DO", "LET", "IN", "END", "OF", "BREAK", "NIL", "FUNCTION",
-	"VAR", "TYPE",
-	//
-	"COMMENT_START", "COMMENT_END"
-]
+// TODO: Consider adding CString lib
 
 func tokname(_ tok: Int32) -> String {
+	let toknames = [
+		"ID", "STRING", "INT", "COMMA", "COLON", "SEMICOLON", "LPAREN",
+		"RPAREN", "LBRACK", "RBRACK", "LBRACE", "RBRACE", "DOT", "PLUS",
+		"MINUS", "TIMES", "DIVIDE", "EQ", "NEQ", "LT", "LE", "GT", "GE",
+		"AND", "OR", "ASSIGN", "ARRAY", "IF", "THEN", "ELSE", "WHILE", "FOR",
+		"TO", "DO", "LET", "IN", "END", "OF", "BREAK", "NIL", "FUNCTION",
+		"VAR", "TYPE",
+		//
+		"COMMENT_START", "COMMENT_END"
+	]
+
 	if tok < 257 || tok > 301 {
 		return "BAD_TOKEN"
 	} else {
@@ -19,27 +20,31 @@ func tokname(_ tok: Int32) -> String {
 	}
 }
 
-//
 typealias CString = UnsafeMutablePointer<Int8>
 
-var path = CommandLine.arguments[1] + "/Chapter 2/"
-var filename = path + "test.tig"
-filename.withCString { EM_reset(CString(mutating: $0)) }
+//
+public func parse(file: String) {
+	let path = CommandLine.arguments[1] + "/"
+	let filename = path + file
+	filename.withCString { EM_reset(CString(mutating: $0)) }
 
-while true {
-	let token = yylex()
-	if token == 0 {
-		break
-	}
+	while true {
+		let token = yylex()
+		if token == 0 {
+			break
+		}
 
-	switch token {
-	case ID:
-		print("ID:     \(tokname(token)) \(EM_tokPos) \(yylval.sval)")
-	case STRING:
-		print("String: \(tokname(token)) \(EM_tokPos) \(yylval.sval)")
-	case INT:
-		print("Int:    \(tokname(token)) \(EM_tokPos) \(yylval.ival)")
-	default:
-		print("Other:  \(tokname(token)) \(EM_tokPos)")
+		switch token {
+		case ID:
+			print("ID:     \(tokname(token)) \(EM_tokPos) \(yylval.sval)")
+		case STRING:
+			print("String: \(tokname(token)) \(EM_tokPos) \(yylval.sval)")
+		case INT:
+			print("Int:    \(tokname(token)) \(EM_tokPos) \(yylval.ival)")
+		default:
+			print("Other:  \(tokname(token)) \(EM_tokPos)")
+		}
 	}
 }
+
+parse(file: "Chapter 2/test.tig")

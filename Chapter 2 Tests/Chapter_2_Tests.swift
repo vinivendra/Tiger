d6bde8fdb619182e57064784c01bd7f4ff23bd70
Vinicius Vendramini <vinivendra@gmail.com>
2017-05-16 11:10:46 -0300

    Improved tests for IDs and types.

--- Chapter 2 Tests/Chapter_2_Tests.swift
+++ Chapter 2 Tests/Chapter_2_Tests.swift
@@ -30,33 +30,34 @@ class Chapter2Tests: XCTestCase {
 	func testIDs() {
 		let tokens = Parser.parse(file: "Chapter 2 Tests/testIDs.tig")
     print(tokens)
-		XCTAssert(tokens.contains(Token(name: "ID",
-		                                position: 33,
-		                                value: "foo")!))
-		XCTAssert(tokens.contains(Token(name: "ID",
-		                                position: 37,
-		                                value: "bar")!))
-		XCTAssert(tokens.contains(Token(name: "ID",
-		                                position: 103,
-		                                value: "baz")!))
-		XCTAssert(tokens.contains(Token(name: "ID",
-		                                position: 107,
-		                                value: "blah")!))
+    let ids: [(position: CInt, value: String)] =
+      [(33, "foo"), (37, "bar"), (103, "baz"), (107, "blah"), (112, "b0lah"),
+       (118, "b0l0ah"), (125, "blah0"), (131, "b0lah_"), (138, "b_lah"),
+       (144, "b_l_ah")]
 
-    XCTAssertEqual(tokens.filter { $0.name == "ID" }.count, 4)
+    for id in ids {
+      XCTAssert(tokens.contains(Token(name: "ID",
+                                      position: id.position,
+                                      value: id.value)!))
+    }
+
+    XCTAssertEqual(tokens.filter { $0.name == "ID" }.count, ids.count)
 	}
 
-  // TODO: Test for Types and IDs with numbers and snake case
   func testTypes() {
-    let tokens = Parser.parse(file: "Chapter 2 Tests/testTypes.tig")
+    let tokens = Parser.parse(file: "Chapter 2 Tests/testIDs.tig")
     print(tokens)
-    XCTAssert(tokens.contains(Token(name: "TYPE",
-                                    position: 113,
-                                    value: "HUE")!))
-    XCTAssert(tokens.contains(Token(name: "TYPE",
-                                    position: 117,
-                                    value: "Hue")!))
 
-    XCTAssertEqual(tokens.filter { $0.name == "TYPE" }.count, 2)
+    let types: [(position: CInt, value: String)] =
+      [(152, "HUE"), (156, "Hue"), (160, "Hue_"), (165, "Hue2"),
+       (170, "Hue2hue"), (178, "H_ue"), (183, "H_u_e"), (189, "Hue_2_hue")]
+
+    for type in types {
+      XCTAssert(tokens.contains(Token(name: "TYPE",
+                                      position: type.position,
+                                      value: type.value)!))
+    }
+
+    XCTAssertEqual(tokens.filter { $0.name == "TYPE" }.count, types.count)
   }
 }

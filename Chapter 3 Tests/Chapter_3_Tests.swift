bbbed71e60f0517e52a1e053401f8943d023dfeb
Vinicius Vendramini <vinivendra@gmail.com>
2017-05-30 10:41:43 -0300

    Added missing files for Ch 3

---
+++ Chapter 3 Tests/Chapter_3_Tests.swift
@@ -0,0 +1,12 @@
+//
+//  Chapter_3_Tests.swift
+//  Chapter 3 Tests
+//
+//  Created by Vinicius Vendramini on 18/05/17.
+//  Copyright © 2017 Vinicius Vendramini. All rights reserved.
+//
+
+import XCTest
+
+class Chapter3Tests: XCTestCase {
+}
